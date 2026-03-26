db2366d817ef0e376c8216a7d4a3df899fd91fe1
Sheshadri08 <sheshadrinanjunda@gmail.com>
2026-03-26 14:26:23 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
